bcec24ac61c894f2d9ac082922d25c313e406b3c
jh136135 <32428573+jh136135@users.noreply.github.com>
2019-06-05 11:18:30 +0800

    Add files via upload
    单链表及相关操作

---
+++ linkedlist.py
@@ -0,0 +1,50 @@
+#定义单链表及相关操作
+
+#自定义异常
+class LinkedListUnderflow(ValueError):
+    pass
+
+class Node():
+    def __init__(self, elem, next_=None):
+        self.elem = elem
+        self.next = next_
+
+class List():
+    def __init__(self):
+        self._head = None
+
+    def is_empty(self):
+        return self._head is None
+
+    def prepend(self, elem): #表头插入结点
+        self._head = Node(elem, self._head)
+
+    def pop(self): #删除表头结点
+        if self._head is None:
+            raise LinkedListUnderflow("pop failed")
+        e = self._head.elem
+        self._head = self._head.next
+        return e
+
+    def append(self, elem):  #表尾插入结点
+        if self._head is None:
+            self._head = Node(elem)
+            return
+        p = self._head
+        while(p is not None):
+            p = p.next
+        p.next = Node(elem)
+
+    def pop_last(self): #删除最后结点
+        if self._head is None:
+            raise LinkedListUnderflow("pop failed")
+        p = self._head
+        if p.next is None:
+            e = p.elem
+            self._head = None
+            return e
+        while p.next.next is not None:
+            p = p.next
+        e = p.next.elem
+        p.next = None
+        return e
